c01c96c642f1c82b0e4316a7db03f51c1c6673a3
Simon <simonjudd2@gmail.com>
2023-05-08 10:41:11 -0600

    big refactor gif replies work

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 import os
 import tweepy
+import yaml
 from twitter_client import fetch_client
+from langchain.llms import OpenAI
 from executor import twitter_executor
 import requests
 
@@ -8,11 +10,14 @@ OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", "")
 SERPER_API_KEY = os.getenv("SERPER_API_KEY", "")
 USER_ID = os.getenv("USER_ID", "")
 
+with open("./params.yaml", "r") as file:
+    params = yaml.safe_load(file)
+
 def main():
    twitterClient = fetch_client()
-   executor = twitter_executor.TwitterExecutor(twitterClient)
-   res  = executor.get_my_timeline(10)
-   print(res)
+   llm = OpenAI(temperature=0.5)
+   executor = twitter_executor.TwitterExecutor(twitterClient, params, llm)
+   executor.reply_to_timeline()
 
 if __name__ == '__main__':
     main()
