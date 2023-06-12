5328e6b5d51405df5da35e5a9b56d1a3eafb7975
Simon <simonjudd2@gmail.com>
2023-06-12 12:44:32 -0600

    add Weaviate vector store

--- src/strategy/strategies/remilio/remilio.py
+++ src/strategy/strategies/remilio/remilio.py
@@ -1,33 +1,37 @@
 import re
+import json
+import weaviate
 from ...base_strategy import TwitterStrategy
 from langchain.chains import LLMChain
 from .remilio_prompt import reply_prompt, tweet_prompt, memory_prompt
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.text_splitter import CharacterTextSplitter
-from langchain.vectorstores import DeepLake
 from langchain.memory import VectorStoreRetrieverMemory
 from langchain.chains import ConversationChain
+from .remilio_agent import RemilioAgent
+from langchain.vectorstores import Weaviate
 
 class RemilioTwitterStrategy(TwitterStrategy):
     def __init__(self, agent_id, llm, params):
         super().__init__(agent_id, llm, params)
-        self.embeddings = OpenAIEmbeddings()
-        self.db = DeepLake(
-            dataset_path="./data/deeplake/remilio",
-            embedding_function=self.embeddings,
-            read_only=True,
-        )
+        self.embeddings = OpenAIEmbeddings(disallowed_special=())
+        self.client = weaviate.Client("http://localhost:8080")
+        self.vectorstore = Weaviate(self.client, "Remilio", "content")
+
 
     def ingest(self, twitterstate):
         text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
         docs = text_splitter.split_documents(twitterstate.list_tweets)
-        #self.db.add_documents(docs)
+        self.vectorstore.add_documents(docs)
+        some_objects = self.client.data_object.get()
+        print(json.dumps(some_objects))
+        self.vectorstore.similarity_search("what are people talking about")
         results = self.process_and_action_tweets(twitterstate.list_tweets)
         return results
 
     def generate_tweet(self, input_text):
         topic = input_text
-        docs = self.db.similarity_search(topic, k=1)
+        docs = self.vectorstore.similarity_search(topic, k=1)
         prompt = memory_prompt
         inputs = [{"context": doc.page_content, "topic": topic} for doc in docs]
         tweet_chain = LLMChain(llm=self.llm, prompt=prompt)
